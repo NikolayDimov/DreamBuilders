70d42f0185d7d89bebc068478cd12ced496fb1f5
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-11-24 10:43:01 +0200

    Register component - add changeHandler

--- src/components/NavbarMenuBtn/auth/Register.jsx
+++ src/components/NavbarMenuBtn/auth/Register.jsx
@@ -12,7 +12,7 @@ function Register() {
     // const [email, setEmail] = useState('');
     // const [password, setPassword] = useState('');
 
-    const [values, setValues] = useState({
+    const [values, setRegisterValues] = useState({
         email: '',
         password: '',
         confirmPassword: ''
@@ -20,6 +20,11 @@ function Register() {
 
 
     console.log(`email: ${values.email}, password: ${values.password}`);
+
+    const changeHandler = (e) => {
+        setRegisterValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    };
+
 
     const handleRegister = async (e) => {
         e.preventDefault();
@@ -41,34 +46,55 @@ function Register() {
                             <h3>Register</h3>
                         </div>
                         <form onSubmit={handleRegister}>
+
                             <div className="form-group first">
                                 <label htmlFor="email">Email</label>
                                 <input
                                     type="text"
                                     className="form-control"
-                                    placeholder="Enter your Email"
+                                    placeholder="Enter your email"
                                     id="email"
                                     name="email"
                                     value={values.email}
+                                    // with Separed State:
                                     // onChange={(e) => setEmail(e.target.value }
-                                    onChange={(e) => setValues({ ...values, email: e.target.value })}
+                                    // with Combined State: 
+                                    // onChange={(e) => setValues({ ...values, email: e.target.value })}
+                                    // with changeHandler: 
+                                    onChange={(e) => changeHandler(e)}
                                 />
-
                             </div>
+
                             <div className="form-group last mb-3">
                                 <label htmlFor="password">Password</label>
                                 <input
                                     type="password"
                                     className="form-control"
-                                    placeholder="Enter your Password"
+                                    placeholder="Enter your password"
                                     id="password"
                                     name="password"
                                     value={values.password}
-                                    onChange={(e) => setValues({ ...values, password: e.target.value })}
-
+                                    // onChange={(e) => setPassword(e.target.value }
+                                    // onChange={(e) => setValues({ ...values, password: e.target.value })}
+                                    onChange={(e) => changeHandler(e)}
                                 />
-
                             </div>
+
+                            <div className="form-group last mb-3">
+                                <label htmlFor="password">Confirm Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Confirm your password"
+                                    id="confirmPassword"
+                                    name="confirmPassword"
+                                    value={values.password}
+                                    // onChange={(e) => setPassword(e.target.value }
+                                    // onChange={(e) => setValues({ ...values, password: e.target.value })}
+                                    onChange={(e) => changeHandler(e)}
+                                />
+                            </div>
+
                             <div className="d-sm-flex mb-5 align-items-center">
                                 <label className="control control--checkbox mb-3 mb-sm-0">
                                     <input type="checkbox" defaultChecked="checked" />
