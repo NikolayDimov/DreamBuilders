ba00f4497f70e861c6549a225fd87b9e2cd78266
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-11-26 00:02:49 +0200

    register validation fix

--- src/components/NavbarMenuBtn/auth/Register.jsx
+++ src/components/NavbarMenuBtn/auth/Register.jsx
@@ -1,35 +1,110 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { useAuth } from '../../../contexts/AuthContext';
 import { Link } from 'react-router-dom';
-
+// import useFormValidation from '../../../utils/useFormValidation';
 import './Register.css';
-
-
 
 
 function Register() {
     const { register } = useAuth();
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
+    // const { errors, validateForm } = useFormValidation();
 
-    const [values, setRegisterValues] = useState({
+    const [values, setValues] = useState({
         email: '',
         password: '',
         confirmPassword: ''
     });
 
 
-    console.log(`email: ${values.email}, password: ${values.password}`);
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [confirmPassError, setConfirmPassError] = useState('');
+
+    // Function to validate email
+    function validateEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        let isEmailValid = true;
+        setEmailError('');
+
+        if (email === '') {
+            setEmailError('Email is required');
+            isEmailValid = false;
+            return isEmailValid;
+        }
+
+        if (!emailRegex.test(email)) {
+            setEmailError('Provide a valid email address');
+            isEmailValid = false;
+            return isEmailValid;
+        }
+
+        return isEmailValid;
+    }
+
+
+    // Function to validate password
+    function validatePassword(pass) {
+
+        let isPasswordValid = true;
+        setPasswordError('');
+
+        if (pass === '') {
+            setPasswordError('Password is required');
+            isPasswordValid = false;
+            return isPasswordValid;
+        }
+
+        if (pass.length < 6 || pass.length > 12) {
+            setPasswordError('Password must be between 6 and 12 characters long');
+            isPasswordValid = false;
+            return isPasswordValid;
+        }
+
+        return isPasswordValid;
+    }
+
+
+    // Function to validate confirmPassword
+    function validateConfirmPassword(confirmPass) {
+
+        let isConfirmPassword = true;
+        setConfirmPassError('');
+
+        if (confirmPass === '') {
+            setConfirmPassError('Confirm Password is required');
+            isConfirmPassword = false;
+            return isConfirmPassword;
+        }
+
+        if (confirmPass != values.password) {
+            setConfirmPassError('Passwords do not match');
+            isConfirmPassword = false;
+            return isConfirmPassword;
+        }
+
+        return isConfirmPassword;
+    }
+
+
 
     const changeHandler = (e) => {
-        setRegisterValues(state => ({ ...state, [e.target.name]: e.target.value }));
+        setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
     };
-
 
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
-            await register(values.email, values.password);
+            let isEmailValid = validateEmail(values.email);
+            let isPasswordValid = validatePassword(values.password);
+            let isConfirmPasswordValid = validateConfirmPassword(values.confirmPassword);
+
+            if (!isEmailValid || !isPasswordValid || !isConfirmPasswordValid) {
+                console.log(`email: ${values.email}`);
+                console.log(`password: ${values.password}`);
+                console.log(`confirmPass: ${values.confirmPassword}`);
+            } else {
+                await register(values.email, values.password);
+            }
         } catch (error) {
             console.error('Registration error:', error);
             console.error('Registration error message:', error.message);
@@ -45,24 +120,21 @@ function Register() {
                         <div className="title">
                             <h3>Register</h3>
                         </div>
-                        <form onSubmit={handleRegister}>
+                        <form onSubmit={handleRegister} noValidate>
 
                             <div className="form-group first">
                                 <label htmlFor="email">Email</label>
                                 <input
-                                    type="text"
+                                    type="email"
                                     className="form-control"
                                     placeholder="Enter your email"
                                     id="email"
                                     name="email"
                                     value={values.email}
-                                    // with Separed State:
-                                    // onChange={(e) => setEmail(e.target.value }
-                                    // with Combined State: 
-                                    // onChange={(e) => setValues({ ...values, email: e.target.value })}
-                                    // with changeHandler: 
-                                    onChange={(e) => changeHandler(e)}
+                                    onChange={changeHandler}
+                                    required
                                 />
+                                {emailError && <p className='error'>{emailError}</p>}
                             </div>
 
                             <div className="form-group last mb-3">
@@ -74,25 +146,25 @@ function Register() {
                                     id="password"
                                     name="password"
                                     value={values.password}
-                                    // onChange={(e) => setPassword(e.target.value }
-                                    // onChange={(e) => setValues({ ...values, password: e.target.value })}
-                                    onChange={(e) => changeHandler(e)}
+                                    onChange={changeHandler}
+                                    required
                                 />
+                                {passwordError && <p className='error'>{passwordError}</p>}
                             </div>
 
                             <div className="form-group last mb-3">
-                                <label htmlFor="password">Confirm Password</label>
+                                <label htmlFor="confirmPassword">Confirm Password</label>
                                 <input
                                     type="password"
                                     className="form-control"
                                     placeholder="Confirm your password"
                                     id="confirmPassword"
                                     name="confirmPassword"
-                                    value={values.password}
-                                    // onChange={(e) => setPassword(e.target.value }
-                                    // onChange={(e) => setValues({ ...values, password: e.target.value })}
-                                    onChange={(e) => changeHandler(e)}
+                                    value={values.confirmPassword}
+                                    onChange={changeHandler}
+                                    required
                                 />
+                                {confirmPassError && <p className='error'>{confirmPassError}</p>}
                             </div>
 
                             <div className="d-sm-flex mb-5 align-items-center">
@@ -102,9 +174,10 @@ function Register() {
                                     <div className="control__indicator" />
                                 </label>
                             </div>
-                            <button className="btn btn-block btn-primary btn-margin" type="submit">Create Account</button>
 
+                            <button className="btn btn-block btn-primary btn-margin" type="submit">Create Account</button>
                         </form>
+
                         <div className="register-link">
                             <p className="login-link">{`You already have an account`}</p>
                             <Link to="/login" className="login-link login-link-to">Login here</Link>
@@ -115,5 +188,6 @@ function Register() {
         </div>
     );
 }
+
 
 export default Register;
