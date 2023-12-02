19b0bd6b7308dca97e07c6aa9241bb5536a8c9e5
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-12-03 00:44:24 +0200

    delete modal blur effect

--- src/components/menu/pages/Delete/Delete.jsx
+++ src/components/menu/pages/Delete/Delete.jsx
@@ -18,9 +18,16 @@ export default function DeleteModal({ onDelete, onCancel, itemToDelete }) {
 
     return (
         <div className={`delete-modal ${isVisible ? 'visible' : 'hidden'}`}>
-            <p>Are you sure you want to delete?</p>
-            <button className="btn btn-outline-dark m-1" onClick={handleConfirm}>Confirm</button>
-            <button className="btn btn-outline-dark m-1" onClick={handleCancel}>Cancel</button>
+            {isVisible && <div className="modal-overlay" onClick={handleCancel} />}
+            <div className="modal-content">
+                <p>Are you sure you want to delete?</p>
+                <button className="btn btn-outline-dark m-1" onClick={handleConfirm}>
+                    Confirm
+                </button>
+                <button className="btn btn-outline-dark m-1" onClick={handleCancel}>
+                    Cancel
+                </button>
+            </div>
         </div>
     );
 }
