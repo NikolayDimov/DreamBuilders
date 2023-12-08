e1575b9c3849a86d90d0c6a5c8ef5bb3375966d6
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-12-08 22:25:17 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [react()],
-// });
-
-// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
 });
