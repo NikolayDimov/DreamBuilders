12b661e9802de105564a572fc01d72c6aea5b5c0
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-12-08 23:32:20 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // plugins: [react()],
-    esbuild: {
-        target: "es2022", // or 'es2021'
-    },
-    plugins: [react({ fastRefresh: false })],
+    plugins: [react()],
+    // esbuild: {
+    //     target: "es2022", // or 'es2021'
+    // },
+    // plugins: [react({ fastRefresh: false })],
     // worker: {
     //     plugins: [react()],
     // },
