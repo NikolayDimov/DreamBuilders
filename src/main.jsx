71cf5db73ed42a391564a50b7bc5dacfaa1a95eb
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-12-08 23:04:47 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({ fastRefresh: false })],
+    plugins: [react()],
+    // plugins: [react({ fastRefresh: false })],
     // worker: {
     //     plugins: [react()],
     // },
